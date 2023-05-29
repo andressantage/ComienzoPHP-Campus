8a13fc99c9dfd645a831460c654dcdfc52395bb2
andressantage <ingeniero.andres.santana@gmail.com>
2023-05-29 17:07:39 -0500

    feat: :sparkles: Parte 5
    Envio resumen de la parte 5 del documento PHP.pdf

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php 
+    /** 5)
+     * !Introducción a php
+     * *Estructura básica de un script PHP
+     * documentRoot es la carpeta principal del servidor Apache donde se deben alojar cada uno de los proyectos que se han creado
+     * Crear las carpetas: css, img, js, scripts,uploads y index.php
+     * **para inicializar y es una buena practica:
+     * ?//<?php 
+     *   ?//Aqui va el codigo
+     * ?//?>
+     */
+
+     /**
+      * !Comando para comprobar el servidor
+      * php -S localhost:3000
+      */
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php echo "Hola"; ?>
+
+    </script>
+</html>
