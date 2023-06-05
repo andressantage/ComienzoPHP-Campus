55ba5b681c9eb7934e3cb4799fa610af07d74f4c
andressantage <ingeniero.andres.santana@gmail.com>
2023-06-05 17:07:20 -0500

    feat: :sparkles: Clase hoy
    Subo lo que se escribio en la clase del 5 de Junio

---
+++ junio5/1.php
@@ -0,0 +1,50 @@
+<?php
+    $nombre;    
+    var_dump(isset($nombre));
+    var_dump(empty($nombre));
+
+    /**
+     * unset() //es para eliminar las variables especificadas
+     */
+?>
+<?php
+
+foreach ($variable as $key => $value) {
+    # code...
+}
+$index = [];
+for ($index = 0; $index < count($lista); $index++) {
+
+}
+while(true){
+
+};
+
+do{
+
+}while(false);
+
+//match
+//La expresión match es similar a una sentencia switch pero tiene algunas diferencias clave:
+$expressionResult = match ($condition) {
+    1, 2 => foo(),
+    3, 4 => bar(),
+    default => baz(),
+};
+
+try {
+    //code...
+} catch (\Throwable $th) {
+    //throw $th;
+}finally{
+
+}
+
+    // $lista = [];
+    // $lista["nombre2"] = "Pepito perez";
+    
+    // if(!isset($lista["nombre"])){
+    //     $lista["nombre"] = "Miguel";
+    // }
+    // print_r($lista);
+?>
---
+++ junio5/main.js
@@ -0,0 +1,35 @@
+//cual es la diferencia entre un ciclo, un bucle y una iteración
+Ciclo: En general, un ciclo es un proceso que se repite continuamente o periódicamente. Puede estar compuesto por una o más iteraciones o bucles. En la programación, el término "ciclo" se usa a menudo para describir estructuras de control específicas que permiten la repetición de un conjunto de instrucciones.
+# Ejemplo de ciclo en Python usando un bucle for
+numeros = [1, 2, 3, 4, 5]
+for numero in numeros:
+    print(numero)
+
+Bucle: Un bucle es una estructura de control que permite ejecutar repetidamente un bloque de código mientras se cumpla una condición específica. El bucle evalúa la condición antes de cada iteración y continúa ejecutando el bloque de código hasta que la condición sea falsa. En esencia, un bucle es una forma de implementar un ciclo en la programación.
+# Ejemplo de bucle en Python usando un bucle while
+contador = 0
+while contador < 5:
+    print(contador)
+    contador += 1
+
+Iteración: Una iteración es una única ejecución de un bucle. Es una vuelta o paso dentro de un ciclo. Cada vez que el bucle se repite, se considera una iteración. Durante cada iteración, se procesan las instrucciones dentro del bloque de código del bucle.
+# Ejemplo de iteración en Python dentro de un bucle
+frutas = ["manzana", "banana", "cereza"]
+for fruta in frutas:
+    print(fruta)
+
+//foreach es una iteración
+array.forEach((value, key) => {
+    
+});
+
+try {
+    
+} catch (error) {
+    
+}finally{
+
+}
+
+throw()
+
